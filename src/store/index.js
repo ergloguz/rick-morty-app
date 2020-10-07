f0bda14ea37b75b3c439d68abf61f36959a125fa
Oğuz Ergül <erguloguz@hotmail.com>
2020-10-07 23:39:22 +0300

    global styles declared

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from "axios"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    characters:[],
+    favorites:[],
   },
   mutations: {
+    SET_CHARACTERS ( state, character ){
+      state.characters = character;
+    },
+    SET_FAVORITE(state, character) {
+      state.favorites.push(character);
+    },
+    REMOVE_FAVORITE(state, movie) {
+      const index = state.favorites.findIndex((item) => item.id === movie.id);
+      state.favorites.splice(index, 1);
+    }
   },
   actions: {
+    loadCharacters({commit}) {
+      axios.get("https://rickandmortyapi.com/api/character")
+          .then((response) => {
+            commit('SET_CHARACTERS', response.data.results)
+          })
+      }
   },
-  modules: {
+  getters: {
+    getCharacters: state => state.characters,
+    getFavorites: (state) => state.favorites,
   }
 })
